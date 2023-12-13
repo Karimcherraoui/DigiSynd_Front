92030f9fd1558168e59dbd6e4e58d5e166a53407
Karimcherraoui <cherraoui.karim96@gmail.com>
2023-12-13 20:06:11 +0100

    Add action types for apartments and authentication

---
+++ src/redux/actions/ActionsType.js
@@ -0,0 +1,16 @@
+export const GET_APARTMENTS = 'GET_APARTMENTS';
+export const ADD_APARTMENT = 'ADD_APARTMENT';
+export const UPDATE_APARTMENT = 'UPDATE_APARTMENT';
+export const DELETE_APARTMENT = 'DELETE_APARTMENT';
+export const PAY_APARTMENT = 'PAY_APARTMENT';
+export const SELECT_APARTMENT = 'SELECT_APARTMENT';
+export const CLEAR_FORM = 'CLEAR_FORM';
+export const SIGN_IN = 'SIGN_IN';
+export const SIGN_OUT = 'SIGN_OUT';
+export const SIGN_UP = 'SIGN_UP';
+export const CLEAR_SELECTEDAPARTMENT = 'CLEAR_SELECTEDAPARTMENT';
+export const GET_ADMIN = 'GET_ADMIN';
+
+
+
+
