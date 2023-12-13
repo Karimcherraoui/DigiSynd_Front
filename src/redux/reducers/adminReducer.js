b883bd01910db33b9be2d1a3d956022dc7d8f3eb
Karimcherraoui <cherraoui.karim96@gmail.com>
2023-12-13 20:06:21 +0100

    Add adminReducer to handle admin-related actions

---
+++ src/redux/reducers/adminReducer.js
@@ -0,0 +1,16 @@
+import * as actionTypes from "../actions/actionsType";
+
+const initialState = {
+  admins: [],
+};
+
+const adminReducer = (state = initialState, action) => {
+  switch (action.type) {
+
+
+    default:
+      return state;
+  }
+};
+
+export default adminReducer;
