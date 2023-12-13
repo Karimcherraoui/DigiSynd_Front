cb73b65570e129b89a564faa287178902526173b
Karimcherraoui <cherraoui.karim96@gmail.com>
2023-12-13 20:03:00 +0100

    Refactor sidebar items component

--- src/components/layout/sideBar/Items.jsx
+++ src/components/layout/sideBar/Items.jsx
@@ -1,53 +1,48 @@
-import { Flex, Icon, Link, Menu, MenuButton ,Text } from '@chakra-ui/react'
+import { Flex, Icon, Link, Menu, MenuButton, Text } from "@chakra-ui/react";
 import { FiHome } from "react-icons/fi";
 import { FaTable } from "react-icons/fa";
 import { MdFormatListBulletedAdd } from "react-icons/md";
-import { CiBoxList } from "react-icons/ci";
 import { CgProfile } from "react-icons/cg";
 import { FaUserPlus } from "react-icons/fa";
-
-
-
-
+import { NavLink } from "react-router-dom";
 
 const icons = {
-    Dashboard: FiHome,
-    Table: FaTable,
-    Add: MdFormatListBulletedAdd,
-    ListApprt: CiBoxList,
-    SignIn : CgProfile,
-    SignUp : FaUserPlus,
-}
+  Dashboard: {
+    link:'/dashboard',
+    icon:FiHome
+  },
+  New_Apartment : {
+    link:'/add-apartment',
+    icon:MdFormatListBulletedAdd
+  },
+  SignIn:{
+    link:'/',
+    icon:CgProfile
+  },
+  SignUp: {
+    link:'/Register',
+    icon:FaUserPlus
+  }
+};
 
-export default function Items({navSize}) {
-
+export default function Items({ navSize }) {
+ 
   return (
-    <Flex
-    mx="10px"
-    flexDirection="column"
-    gap={6}
-    alignItems={navSize === "small" ? "center" : "flex-start"}
-    >
-        <Menu placement = "right">
+    <Flex mx="10px" flexDirection="column" gap={6} alignItems={navSize === "small" ? "center" : "flex-start"}>
+      <Menu placement="right">
         {Object.entries(icons).map(([key, value]) => (
-            <Link key={key}
-            // p ={3}
-            borderRadius={8}
-            _hover={{ textDecor: "none" , backgroundColor: "blue.900" }}
-            w = {navSize == "large" && "80%"}
-            >
-            <MenuButton>
-                <Flex p={3} gap = "10px" alignItems="center" >
-                    <Icon color="blue.300" as = {value}     />
-                    <Text color="gray.400" display={navSize === "small" ? "none" : "flex" }>{key}</Text>
-                </Flex>
+          <Link as={NavLink} to={value.link}  key={key} borderRadius={8} _hover={{ textDecor: "none", backgroundColor: "blue.900" }} w={navSize == "large" && "100%"}>
+            <MenuButton >
+              <Flex p={3} gap="10px" alignItems="center">
+                <Icon color="blue.300" as={value.icon} />
+                <Text color="gray.400" display={navSize === "small" ? "none" : "flex"}>
+                  {key}
+                </Text>
+              </Flex>
             </MenuButton>
-            </Link>
-
-))}
-        </Menu>
-
+          </Link>
+        ))}
+      </Menu>
     </Flex>
-
-  )
+  );
 }
