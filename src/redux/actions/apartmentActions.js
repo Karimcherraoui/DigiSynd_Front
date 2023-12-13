0c62f6dc64efdec25df3c92796e3ded5fb2a62d6
Karimcherraoui <cherraoui.karim96@gmail.com>
2023-12-13 20:06:17 +0100

    Add apartment actions and update payApartment action

---
+++ src/redux/actions/apartmentActions.js
@@ -0,0 +1,85 @@
+import * as actionTypes from './actionsType'
+import ApiService from '../../services/api';
+
+export const getApartments = () => async (dispatch) => {
+    try {
+      const apartments = await ApiService.getApartments();
+      console.log("from apart action " ,apartments);
+      dispatch({ type: actionTypes.GET_APARTMENTS, payload: apartments });
+
+    } catch (error) {
+    //   enqueueSnackbar("Error liking post ", { variant: 'error' })
+      console.error('Error fetching apartments:', error);
+    }
+  };
+
+
+
+//   export const clearSelectedPost = () => {
+//     return {
+//       type: actionTypes.CLEAR_SELECTEDPOST,
+//     };
+//   }
+  
+
+// export const addPost = (data) => async (dispatch) => {
+
+//   try {
+//     const newPost = await ApiService.createPost({...data });
+//     dispatch({ type: actionTypes.ADD_POST, payload: newPost });
+//     // enqueueSnackbar("Post has been added successfully.", { variant: 'success' })
+
+//   } catch (error) {
+//     // enqueueSnackbar("Error adding post.", { variant: 'error' })
+
+//   }
+// };
+
+// export const deletePost = (postId) => async (dispatch) => {
+//   try {
+//     await ApiService.deletePost(postId);
+//     dispatch({ type: actionTypes.DELETE_POST, payload: postId });
+//     // enqueueSnackbar("Post deleted successfully.", { variant: 'success' })
+
+//   } catch (error) {
+//     // enqueueSnackbar("Error deleting post.", { variant: 'error' })
+//   }
+// };
+
+
+
+// export const updatePost = ({_id:postId , ...post} ) => async (dispatch) => {
+//   try {
+//    const updatedPost = await ApiService.updatePost(postId, post);
+//     dispatch({ type: actionTypes.UPDATE_POST, payload: updatedPost });
+//     // enqueueSnackbar("Post updated successfully.", { variant: 'success' })
+
+//   } catch (error) {
+//     enqueueSnackbar("Error updating post.", { variant: 'error' })
+
+//   }
+// };
+
+// export const selectPost =  (post) => {
+// //   enqueueSnackbar("Post is selected", { variant: 'success' })
+
+//   return {
+    
+//     type: actionTypes.SELECT_POST,
+//     payload: post
+//   }
+// };
+
+
+
+export const payApartment = (apartmentID) => async (dispatch) => {
+  try {
+   const apartment = await ApiService.payApartment(apartmentID );
+    dispatch({ type: actionTypes.PAY_APARTMENT, payload: {apartmentID} });
+    // enqueueSnackbar("Post is liked", { variant: 'success' })
+
+  } catch (error) {
+//   enqueueSnackbar("Error liking post ", { variant: 'error' })
+console.error('Error fetching aparts:', error);
+  }
+};
