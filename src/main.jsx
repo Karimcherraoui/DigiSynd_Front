38f3128201e4de87e91bf5fcba041473d21412e8
Karimcherraoui <cherraoui.karim96@gmail.com>
2023-12-15 12:09:18 +0100

    Add Redux store provider to App component

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import store from './redux/store/store'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+        <Provider store={store}>
 
         <ChakraProvider>
         <App />
         </ChakraProvider>
+    </Provider>
+
 ,
 )
