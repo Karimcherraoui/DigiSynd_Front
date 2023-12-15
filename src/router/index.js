fdb232eb2392148cfa67786b8844f9de95cd9873
Karimcherraoui <cherraoui.karim96@gmail.com>
2023-12-15 12:23:41 +0100

    Add router configuration for authentication and dashboard

---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import { createBrowserRouter } from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
+import ApartmentForm from "../components/forms/ApartmentForm";
+import ListApart from "../components/ListApart/ListApart";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import PrivateRoute from "./privateRoute";
+
+/**
+ * @type {import("react-router-dom").RouteObject[]}
+ */
+
+const routes = [
+  {
+    Component: PrivateRoute,
+    children: [
+      {
+        Component: Dashboard,
+        children: [
+          {
+            path: "/dashboard",
+            Component: ListApart,
+          },
+          {
+            path: "/add-apartment",
+            Component: ApartmentForm,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: "/",
+    Component: Login,
+  },
+  {
+    path: "/Register",
+    Component: Register,
+  },
+];
+
+const router = createBrowserRouter(routes);
+
+export default router;
