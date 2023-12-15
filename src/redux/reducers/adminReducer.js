2e17e858dac1700e7e2e2b0754e63e4369069b67
Karimcherraoui <cherraoui.karim96@gmail.com>
2023-12-15 12:23:35 +0100

    Refactor adminReducer to handle login and logout actions

--- src/redux/reducers/adminReducer.js
+++ src/redux/reducers/adminReducer.js
@@ -1,12 +1,23 @@
 import * as actionTypes from "../actions/actionsType";
 
 const initialState = {
-  admins: [],
+  admin: null,
 };
 
 const adminReducer = (state = initialState, action) => {
   switch (action.type) {
-
+    case actionTypes.LOGIN:
+      console.log("from admin reducer", action.payload);
+      return {
+        ...state,
+        admin: action.payload,
+      };
+      case actionTypes.LOGOUT:
+      return {
+        ...state,
+        admin: null,
+      
+      };
 
     default:
       return state;
