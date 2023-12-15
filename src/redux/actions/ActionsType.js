1a9b1c9c0a9f60348b5b9e95d189cb433458b6e7
Karimcherraoui <cherraoui.karim96@gmail.com>
2023-12-15 12:23:14 +0100

    Delete unused files and update action types

--- src/redux/actions/ActionsType.js
+++ src/redux/actions/ActionsType.js
@@ -6,9 +6,9 @@ export const DELETE_FACTURE = 'DELETE_FACTURE';
 export const PAY_APARTMENT = 'PAY_APARTMENT';
 export const SELECT_APARTMENT = 'SELECT_APARTMENT';
 export const CLEAR_FORM = 'CLEAR_FORM';
-export const SIGN_IN = 'SIGN_IN';
-export const SIGN_OUT = 'SIGN_OUT';
-export const SIGN_UP = 'SIGN_UP';
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'SIGN_OUT';
+export const REGISTER = 'REGISTER';
 export const CLEAR_SELECTEDAPARTMENT = 'CLEAR_SELECTEDAPARTMENT';
 export const GET_ADMIN = 'GET_ADMIN';
 
