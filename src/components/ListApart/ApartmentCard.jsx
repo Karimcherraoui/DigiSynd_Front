7bc387203e058af7503e7019eea37d426a551e32
Karimcherraoui <cherraoui.karim96@gmail.com>
2023-12-15 12:21:09 +0100

    Fix formatting and update dialogs in ApartmentCard component

--- src/components/ListApart/ApartmentCard.jsx
+++ src/components/ListApart/ApartmentCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import { Button, Flex, Image, Text, useDisclosure } from "@chakra-ui/react";
 import img from "../../assets/images/apart.webp";
 import { LuPenSquare } from "react-icons/lu";
@@ -6,23 +6,22 @@ import { MdDeleteOutline } from "react-icons/md";
 import { GiReceiveMoney } from "react-icons/gi";
 import { FaRegFilePdf } from "react-icons/fa6";
 import DeleteConfirm from "../dialogs/DeleteConfirm";
-import { useDispatch } from 'react-redux';
-import { payApartment } from '../../redux/actions/apartmentActions';
 
-export default function ApartmentCard({apartment}) {
-  console.log(apartment);
-  const dispatch = useDispatch();
-    const { isOpen, onOpen, onClose } = useDisclosure()
+import Confirmation from "../dialogs/confirmation";
 
-    const handlPayment = () => {
-      dispatch(payApartment(apartment._id))
-    }
+export default function ApartmentCard({ apartment }) {
+  const deleteDialog = useDisclosure();
+  const paymentDialog = useDisclosure();
+  const handleClickPay = () => {
+    paymentDialog.onOpen();
+  };
+  const handleClickDelete = () => {
+    deleteDialog.onOpen();
+  };
 
 
-
-
-    return ( 
-        <Flex
+  return (
+    <Flex
       flexDirection="column"
       m="30px"
       overflow="hidden"
@@ -33,13 +32,28 @@ export default function ApartmentCard({apartment}) {
       boxShadow="rgba(0, 0, 0, 0.35) 20px 20px 30px"
       position="relative"
     >
-      <DeleteConfirm id={apartment._id} isOpen={isOpen} onClose={onClose} />
+      <DeleteConfirm
+        id={apartment._id}
+        isOpen={deleteDialog.isOpen}
+        onClose={deleteDialog.onClose}
+      />
+      <Confirmation
+        id={apartment._id}
+        isOpen={paymentDialog.isOpen}
+        onClose={paymentDialog.onClose}
+      />
+
       <Flex pos="absolute" gap="10px" right="10px" top="10px">
         <Button size="sm" colorScheme="blue" variant="solid">
           <LuPenSquare />
         </Button>
-  
-        <Button size="sm" colorScheme="red" variant="solid" onClick={onOpen}>
+
+        <Button
+          size="sm"
+          colorScheme="red"
+          variant="solid"
+          onClick={handleClickDelete}
+        >
           <MdDeleteOutline />{" "}
         </Button>
       </Flex>
@@ -47,8 +61,9 @@ export default function ApartmentCard({apartment}) {
         src={img}
         w="100%"
         h="50%"
-        boxShadow="rgba(0, 0, 0, 0.35) 2px 2px 10px" />
-  
+        boxShadow="rgba(0, 0, 0, 0.35) 2px 2px 10px"
+      />
+
       <Flex
         flexDirection="column"
         w="100%"
@@ -58,53 +73,44 @@ export default function ApartmentCard({apartment}) {
         p="10px"
       >
         <Text size="sm" color="black" w="100%">
-          <span
-            style={{ width: "100%", height: "1px", fontWeight: "bold" }}
-          >
+          <span style={{ width: "100%", height: "1px", fontWeight: "bold" }}>
             Owner :{" "}
           </span>
           {apartment.firstNameOwner} {apartment.lastNameOwner}
         </Text>
         <Text color="black">
           {" "}
-          <span
-            style={{ width: "100%", height: "1px", fontWeight: "bold" }}
-          >
+          <span style={{ width: "100%", height: "1px", fontWeight: "bold" }}>
             Number :{" "}
           </span>
           {apartment.numberApart}
         </Text>
         <Text color="black">
           {" "}
-          <span
-            style={{ width: "100%", height: "1px", fontWeight: "bold" }}
-          >
+          <span style={{ width: "100%", height: "1px", fontWeight: "bold" }}>
             Floor :{" "}
           </span>
           {apartment.floor}
         </Text>
         <Text color="black">
           {" "}
-          <span
-            style={{ width: "100%", height: "1px", fontWeight: "bold" }}
-          >
-            Phone : {" "}
+          <span style={{ width: "100%", height: "1px", fontWeight: "bold" }}>
+            Phone :{" "}
           </span>
           {apartment.phone}
         </Text>
       </Flex>
-  
+
       <Flex gap="10px" py="15px" justifyContent="center">
         <Button
-        onClick={handlPayment}
+          onClick={handleClickPay}
           leftIcon={<GiReceiveMoney />}
           w="70%"
           colorScheme={apartment.isPaid ? "gray" : "green"}
           variant="solid"
           isDisabled={apartment.isPaid}
-
         >
-          {apartment.isPaid ? "Payed" : "Pay"}
+          {apartment.isPaid ? "Paid" : "Pay"}
         </Button>
         <Button
           w="20%"
@@ -116,7 +122,5 @@ export default function ApartmentCard({apartment}) {
         </Button>
       </Flex>
     </Flex>
-    )
-  }
-  
-  
+  );
+}
