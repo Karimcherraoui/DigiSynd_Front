17defe27206e7d33fd572a8b2328b78658d9edf0
Karimcherraoui <cherraoui.karim96@gmail.com>
2023-12-15 12:21:05 +0100

    Add new sidebar item and implement logout functionality

--- src/components/layout/sideBar/SideBar.jsx
+++ src/components/layout/sideBar/SideBar.jsx
@@ -1,20 +1,47 @@
 import {
   Avatar,
+  Button,
   Divider,
   Flex,
   Heading,
+  Icon,
   IconButton,
+  MenuButton,
   Text,
 } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Items from "./items";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { FaUserPlus } from "react-icons/fa";
+import { CiLogout } from "react-icons/ci";
+import { logout } from "../../../redux/actions/adminActions";
+
+
+const NewSyndic = {
+  link: "/Register",
+  icon: FaUserPlus,
+  name: "New Syndic",
+};
 
 export default function SideBar() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = JSON.parse(localStorage.getItem("syndic"));
+  const syndic = user && user.admin;
+  const userSyndic = useSelector((state) => state.admin.admin);
   const [navSize, setNaveChange] = useState("large");
   const MenuToggle = () => {
     navSize === "large" ? setNaveChange("small") : setNaveChange("large");
   };
+
+  const handleLogout = () => {
+    dispatch(logout())
+    navigate('/')
+
+  }
+  useEffect(() => {}, [userSyndic]);
 
   return (
     <Flex
@@ -28,6 +55,7 @@ export default function SideBar() {
       background="linear-gradient(112deg, #060B26 50.3%, rgba(26, 31, 55, 0.70) 100%)"
       backdropFilter="blur(60px)"
       transition="width 0.4s ease-in-out"
+      alignItems="center"
     >
       <Text
         color="white"
@@ -40,7 +68,6 @@ export default function SideBar() {
         textAlign="center"
         bgGradient="linear-gradient(98deg, #FFF 70.67%, rgba(117, 122, 140, 0.00) 108.55%)"
         bgClip="text"
- 
       >
         DigiSynd
       </Text>
@@ -58,20 +85,66 @@ export default function SideBar() {
       <Items navSize={navSize} />
 
       <Flex p="5%" flexDir="column" w="100%" alignItems="flex-start" mb={4}>
+        {syndic && syndic.role === "superAdmin" && (
+          <Flex mb="10px">
+            <Link
+              as={NavLink}
+              to={NewSyndic.link}
+              borderRadius={8}
+              _hover={{ textDecor: "none", backgroundColor: "blue.900" }}
+              w={navSize == "large" && "100%"}
+            >
+              <Flex p={3} gap="10px" alignItems="center">
+                <Icon color="blue.300" as={NewSyndic.icon} />
+                <Text
+                  color="gray.400"
+                  display={navSize === "small" ? "none" : "flex"}
+                >
+                  {NewSyndic.name}
+                </Text>
+              </Flex>
+            </Link>
+          </Flex>
+        )}
         <Divider display={navSize == "small" ? "none" : "flex"} />
+
+
         <Flex mt={4} align="center">
-          <Avatar size="sm" src="avatar-1.jpg" ml="7px" />
+          <Avatar
+            ml="8px"
+            size="sm"
+            name={syndic ? syndic.fullName : ""}
+
+            // src="https://bit.ly/tioluwani-kolawole"
+          />{" "}
           <Flex
             flexDir="column"
             ml={4}
             display={navSize == "small" ? "none" : "flex"}
           >
             <Heading as="h3" size="xs" color="white">
-              Karim Cherraoui
+              {syndic ? syndic.fullName : "Not Connected"}
             </Heading>
           </Flex>
         </Flex>
+
+          <Flex ml={navSize == "small" ? "none" : "30px"} display={syndic ? "block" : "none"}> 
+
+        <Button background="red" position="absolute" bottom="5" onClick={handleLogout}>
+        <CiLogout color="white"/>
+        <Flex
+          flexDir="column"
+          ml={4}
+          display={navSize == "small" ? "none" : "flex"}
+          color="white"
+          >
+          
+          Logout
+        </Flex>
+      </Button>
+          </Flex>
       </Flex>
+      
     </Flex>
   );
 }
