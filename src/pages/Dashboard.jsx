e6c00876cb78baae99c1665f52c7698d8c8267cf
Karimcherraoui <cherraoui.karim96@gmail.com>
2023-12-15 12:21:20 +0100

    Refactor Dashboard component and update BoxStat titles

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,26 +1,23 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import Background from "../assets/images/Background.png";
 import BoxStat from "../components/ListApart/BoxStat";
 
-import { FaUsers } from "react-icons/fa";
 import { GiTakeMyMoney } from "react-icons/gi";
 import Footer from "../components/layout/Footer";
 import SideBar from "../components/layout/sideBar/SideBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { MdApartment } from "react-icons/md";
 import { FaMoneyBillTransfer } from "react-icons/fa6";
+import { useEffect, useLayoutEffect } from "react";
 
 export default function Dashboard() {
+  const aparts = useSelector((state) => state.aparts.apartments);
+  const paidAparts = aparts.filter((apart) => apart.isPaid === true);
+  const numberOfApartments = aparts.length;
+  const numberOfPaidApartments = paidAparts.length;
+  const numberOfNotPaidApartments = numberOfApartments - numberOfPaidApartments;
 
-  const aparts  = useSelector((state) => state.aparts.apartments);
-  const payedAparts = aparts.filter((apart) => apart.isPaid === true);
-
-
-
-const numberOfApartments = aparts.length;
-const numberOfPayedApartments = payedAparts.length;
-const numberOfNotPayedApartments = numberOfApartments - numberOfPayedApartments;
 
   return (
     <Flex
@@ -33,26 +30,26 @@ const numberOfNotPayedApartments = numberOfApartments - numberOfPayedApartments;
     >
       <SideBar />
 
-      <Flex flexDirection="column" mt="25px" mx="auto" >
-        <Flex gap="150px" justifyContent="center" >
+      <Flex flexDirection="column" mt="25px" mx="auto">
+        <Flex gap="150px" justifyContent="center">
           <BoxStat
             title="Number of Apartments"
             number={numberOfApartments}
             icon={<MdApartment color="white" size="20px" />}
           />
           <BoxStat
-            title="Apartments payed"
-            number={numberOfPayedApartments}
+            title="Apartments paid"
+            number={numberOfPaidApartments}
             icon={<GiTakeMyMoney color="white" size="20px" />}
-            />
+          />
           <BoxStat
-            title="Apartments not payed"
-            number={numberOfNotPayedApartments}
+            title="Apartments not paid"
+            number={numberOfNotPaidApartments}
             icon={<FaMoneyBillTransfer color="white" size="20px" />}
           />
         </Flex>
 
-        <Outlet/>
+        <Outlet />
 
         <Footer />
       </Flex>
