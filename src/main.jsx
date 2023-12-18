9cc8c4e3126436d89dcf15e0a27867c5b12b5267
Karimcherraoui <cherraoui.karim96@gmail.com>
2023-12-18 16:50:38 +0100

    Remove unnecessary code

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,9 @@ import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
         <Provider store={store}>
-
         <ChakraProvider>
         <App />
         </ChakraProvider>
     </Provider>
 
-,
 )
